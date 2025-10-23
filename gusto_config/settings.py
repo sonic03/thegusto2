702e6626ed382b0b907b6f4540b37fedf0b66c7a
sonic03 <mrkayacik@yahoo.com>
2025-10-23 11:22:47 +0300

    ssl düzenlemesi

--- gusto_config/settings.py
+++ gusto_config/settings.py
@@ -33,6 +33,10 @@ DEBUG = False
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 # email entegrasyonu
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
